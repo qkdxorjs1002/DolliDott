6382de5aeb29b6510b1e52b68eea740b4e9cc5e5
Novang <qkdxorjs1002@naver.com>
2020-09-30 07:28:52 +0900

    Add invite command

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import discord
 from discord.ext import commands, tasks
 
 _TOKEN = os.environ['DOLLIDOTT_TOKEN']
-cmd_prefix = '!'
 
-cmd_list = {'help': '명령어', 'lyrics': '가사', 'crazy': '테러'}
+cmd_prefix = '!'
+cmd_list = {'help': '명령어', 'invite': '초대', 'lyrics': '가사', 'crazy': '테러'}
 cmd_help = {'명령어': '이 메시지를 띄울 수 있는 명령어에요.', 'lyrics': '돌리랑 도트 가사를 볼 수 있는 명령어에요.', '테러': '돌리랑 도트가 그렇게 좋아? 그럼 계속 불러!'}
 
 comments_lyrics = ['> 돌리랑~ 도트가~ 제일~ 좋아~:musical_note:',
@@ -28,11 +28,19 @@ async def on_ready():
 # 명령 "명령어"
 @bot.command(name=cmd_list['help'])
 async def help_me(ctx):
-    message = '> 사용 가능한 명령어 목록\n```'
+    message = '> :llama: **사용 가능한 명령어 목록**\n```'
     for cmd in cmd_list:
         message += ':round_pushpin: ' + cmd_prefix + cmd + ' ' + cmd_help[cmd] + '\n\n'
     message += '```'
 
+    await ctx.send(message)
+
+
+# 명령 "초대"
+@bot.command(name=cmd_list['invite'])
+async def lyrics(ctx):
+    message = '> :llama: **돌리랑 도트를 다른 서버에 초대해보세요!**\n' + \
+              '```https://discord.com/oauth2/authorize?client_id=' + str(bot.user.id) + '&scope=bot```'
     await ctx.send(message)
 
 
@@ -64,9 +72,9 @@ async def crazy(ctx, user: discord.User, cnt=1):
 async def crazy_error(ctx, error):
     print(error)
     if '50007' in error:
-        await ctx.send('> **에구구**\n**테러를 받은 유저가 저를 차단했어요...칫...:put_litter_in_its_place:**')
+        await ctx.send('> :llama: **에구구**\n**테러를 받은 유저가 저를 차단했어요...칫...:put_litter_in_its_place:**')
     else:
-        await ctx.send('> **명령어 \"테러\" 사용법**\n**멈출 수 없으니 신중히 사용하세요!**\n```!테러 @<유저명> <반복 횟수(최대20)>```')
+        await ctx.send('> :llama: **명령어 \"테러\" 사용법**\n**멈출 수 없으니 신중히 사용하세요!**\n```!테러 @<유저명> <반복 횟수(최대20)>```')
 
 
 bot.run(_TOKEN)
