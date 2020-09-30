0019a078b0498f2a8856f9385351b5bf540dc234
Novang <qkdxorjs1002@naver.com>
2020-09-30 19:35:57 +0900

    Add make_message

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ from discord.ext import commands, tasks
 _TOKEN = os.environ['DOLLIDOTT_TOKEN']
 
 cmd_prefix = '!'
-cmd_list = {'help': '명령어', 'invite': '초대', 'lyrics': '가사', 'crazy': '테러'}
-cmd_help = {'help': '이 메시지를 띄울 수 있는 명령어에요.',
-            'invite': '돌리랑 도트를 다른 서버에 초대해보세요.',
-            'lyrics': '돌리랑 도트 가사를 볼 수 있는 명령어에요.',
-            'crazy': '돌리랑 도트가 그렇게 좋아? 그럼 계속 불러!'}
+cmd_list = {'help': ['명령어', '이 메시지를 띄울 수 있는 명령어에요.'],
+            'invite': ['초대', '돌리랑 도트를 다른 서버에 초대해보세요.'],
+            'lyrics': ['가사', '돌리랑 도트 가사를 볼 수 있는 명령어에요.'],
+            'crazy': ['테러', '돌리랑 도트가 그렇게 좋아? 그럼 계속 불러!']}
 
 comments_lyrics = ['> 돌리랑~ 도트가~ 제일~ 좋아~:musical_note:',
                    '> 돌리랑~ 도트가~ 제일~~~ 쪼아~:musical_note:',
@@ -22,6 +21,17 @@ comments_lyrics = ['> 돌리랑~ 도트가~ 제일~ 좋아~:musical_note:',
 bot = commands.Bot(command_prefix=cmd_prefix)
 
 
+def make_message(title, sub, contents, contents_type='context'):
+    message = '> :llama: **' + title + '**\n\n**' + sub + '**\n'
+
+    if contents_type == 'context':
+        message += '>>> ' + contents
+    elif contents_type == 'command':
+        message += '```' + contents + '```'
+
+    return message
+
+
 @bot.event
 async def on_ready():
     message = 'Logged in as ' + bot.user.name + ', id: ' + str(bot.user.id) + '\n'
@@ -31,9 +41,13 @@ async def on_ready():
 # 명령 "명령어"
 @bot.command(name=cmd_list['help'])
 async def help(ctx):
-    message = '> :llama: **사용 가능한 명령어 목록**\n\n>>> '
+    contents = ''
+
     for cmd, val in cmd_list.items():
-        message += ':round_pushpin: ' + cmd_prefix + val + '\t' + cmd_help[cmd] + '\n\n'
+        contents += ':round_pushpin: ' + cmd_prefix + val[0] + '\t' + val[1] + '\n\n'
+
+    message = make_message('사용 가능한 명령어 목록', '',
+                           contents, 'command')
 
     await ctx.send(message)
 
@@ -41,8 +55,9 @@ async def help(ctx):
 # 명령 "초대"
 @bot.command(name=cmd_list['invite'])
 async def invite(ctx):
-    message = '> :llama: **돌리랑 도트를 다른 서버에 초대해보세요!**\n' + \
-              '```https://discord.com/oauth2/authorize?client_id=' + str(bot.user.id) + '&scope=bot```'
+    message = make_message('돌리랑 도트를 다른 서버에 초대해보세요!', '',
+                           'https://discord.com/oauth2/authorize?client_id=' + str(bot.user.id) + '&scope=bot')
+
     await ctx.send(message)
 
 
@@ -57,8 +72,9 @@ async def lyrics(ctx):
 # 명령 "테러"
 @bot.command(name=cmd_list['crazy'])
 async def crazy(ctx, user: discord.User, cnt=1):
-    message = '> ' + user.display_name + ': \"돌리랑~ 도트가~ 제일~ 좋아~:musical_note:\"'
-    await ctx.send(bot.user.avatar_url)
+    message = make_message(bot.user.avatar_url, '',
+                           user.display_name + comments_lyrics[0])
+
     await ctx.send(message)
 
     if cnt > 20:
@@ -74,9 +90,15 @@ async def crazy(ctx, user: discord.User, cnt=1):
 async def crazy_error(ctx, error):
     print(error)
     if '50007' in str(error):
-        await ctx.send('> :llama: **에구구**\n**테러를 받은 유저가 저를 차단했어요...칫...:put_litter_in_its_place:**')
+        message = make_message('에구구', '',
+                               '테러를 받은 유저가 저를 차단했어요...칫...:put_litter_in_its_place:')
+
+        await ctx.send(message)
     else:
-        await ctx.send('> :llama: **명령어 \"테러\" 사용법**\n**멈출 수 없으니 신중히 사용하세요!**\n```!테러 @<유저명> <반복 횟수(최대20)>```')
+        message = make_message('명령어 \"테러\" 사용법', '멈출 수 없으니 신중히 사용하세요!',
+                               '!테러 @<유저명> <반복 횟수(최대20)>', 'command')
+
+        await ctx.send(message)
 
 
 bot.run(_TOKEN)
